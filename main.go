b59ec26de61e5c2b8d5e07dc5f2ed68fc3342723
Sam Myres <samiam2013@gmail.com>
2022-03-26 01:04:17 +0000

    updated service

--- main.go
+++ main.go
@@ -14,6 +14,14 @@ func main(){
 }
 
 func narcissus( w http.ResponseWriter, r *http.Request ){
+
+	// Loop over header names
+for name, values := range r.Header {
+    // Loop over all values for the name.
+    for _, value := range values {
+        fmt.Println(name, value)
+    }
+}
 	_, err := w.Write([]byte("hello world"))
 	if err != nil {
 		log.Fatal(err.Error())
