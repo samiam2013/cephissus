8c36b130276676f72b38aa500b49860a5b633904
Samuel <samiam2013@gmail.com>
2022-03-26 01:42:29 +0000

    post the actual echo code

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -13,18 +16,48 @@ func main() {
 	}
 }
 
+// Req allows for marshalling the request data for JSON
+type Req struct {
+	Headers map[string]string `json:"headers"`
+	Body    string            `json:"body"`
+}
+
 func narcissus(w http.ResponseWriter, r *http.Request) {
+	req := Req{
+		Headers: map[string]string{},
+		Body:    "",
+	}
 
 	// Loop over header names
 	for name, values := range r.Header {
 		// Loop over all values for the name.
 		for _, value := range values {
-			log.Println(name, value)
+			req.Headers[name] = value
 		}
 	}
-	_, err := w.Write([]byte("hello world"))
+
+	bytes, err := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	r.Body.Close()
+	req.Body = string(bytes)
+
+	toWrite, err := json.Marshal(req)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	strRepLen := strconv.Itoa(len(toWrite))
+	w.Header().Add("x-powered-by", "narcissus()")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Content-Length", strRepLen)
+	written, err := w.Write(toWrite)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	if written != len(toWrite) {
+		log.Fatal("written length not equal to length of write content (unthinkable)")
+	}
 }
