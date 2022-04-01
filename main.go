775bbed904257229b0bd5a8c881bfc4b4b0777ce
Samuel <samiam2013@gmail.com>
2022-04-01 01:47:29 +0000

    create & serve /testForm, add multipart form parse

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 )
 
 const defaultPort = ":3000"
@@ -14,6 +17,7 @@ const debugPort = ":80"
 
 func main() {
 	http.HandleFunc("/", narcissus)
+	http.HandleFunc("/testForm", testForm)
 	//debug := false
 	port := defaultPort
 	if _, err := os.Stat(".debug"); err == nil {
@@ -60,6 +64,17 @@ func narcissus(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err.Error())
 	}
 	for k, v := range r.PostForm {
+		if isFile(v) {
+			var (
+				b64 string
+				err error
+			)
+			if b64, err = parseFile(r, k); err == nil {
+				req.Post[k] = append(req.Post[k], b64)
+				continue
+			}
+			log.Println(err.Error())
+		}
 		req.Post[k] = append(req.Post[k], v...)
 	}
 
@@ -89,4 +104,45 @@ func narcissus(w http.ResponseWriter, r *http.Request) {
 	if written != len(toWrite) {
 		log.Fatal("written length not equal to length of write content (unthinkable)")
 	}
+}
+
+func testForm(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./htdocs/index.html")
+}
+
+// isFile checks that there is a single argument and it looks like a file
+func isFile(postArg []string) bool {
+	if len(postArg) != 1 {
+		return false
+	}
+	split := strings.Split(postArg[0], ".")
+	if len(split) < 2 {
+		return false
+	}
+	// this is restrictive and doesn't include .js, .ts etc. on purpose
+	exts := map[string]bool{
+		"css":  true,
+		"html": true,
+		"xml":  true,
+		"json": true,
+		"jpg":  true,
+		"jpeg": true,
+		"png":  true,
+		"gif":  true,
+	}
+	_, ok := exts[split[len(split)-1]]
+	return ok
+}
+
+// parseFile unpacks multi-part form data into a b64 string
+func parseFile(r *http.Request, formFilename string) (string, error) {
+	in, _, err := r.FormFile("file")
+	if err != nil {
+		return "", err
+	}
+	defer in.Close()
+
+	out := bytes.NewBuffer([]byte{})
+	io.Copy(out, in)
+	return out.String(), nil
 }
